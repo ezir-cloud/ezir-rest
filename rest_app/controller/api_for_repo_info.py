58d7d30bcb640458c9e7b5d05591a4d89867b05c
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2020-06-30 12:26:13 +0530

    controller program

---
+++ rest_app/controller/api_for_repo_info.py
@@ -0,0 +1,18 @@
+from _operator import index
+
+from githubapis.search import GitRepositoryApisDetails
+
+from rest_app.dal.dal import insert_api_repo_info
+
+
+def api_for_repo_details(reponame):
+    github_api_repo_details = GitRepositoryApisDetails()
+    repo_info = github_api_repo_details.search_repository_details(reponame)
+    return repo_info
+
+
+def insert_repo_details(api_repo_info):
+
+    index='github_repo_details'
+    repo_info = insert_api_repo_info(api_repo_info,index)
+    return repo_info
