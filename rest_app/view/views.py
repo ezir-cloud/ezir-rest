2f4a45fb2fc654a6286df147d357fe6f230513c3
maazil3861 <maazshaikh7020@gmail.com>
2020-07-01 16:09:26 +0530

    It take repo and file name for insert and take file name  for search file

--- rest_app/view/views.py
+++ rest_app/view/views.py
@@ -1,10 +1,11 @@
+from elasticsearch import Elasticsearch
 from flask import Flask, jsonify, request
 from githubapis.search import GithubRepoApis
 from githubapis.search import Github
-from rest_app.controller.api_for_file_info import api_for_file_details, insert_file_details
+from rest_app.controller.api_for_file_info import api_for_file_details, insert_file_details, search_file_details
 
 app = Flask(__name__)
-
+es = Elasticsearch()
 
 @app.route('/files-info', methods=['POST'])
 def file_details():
@@ -13,8 +14,14 @@ def file_details():
     filename = request.args.get('filename')
     file_details = api_for_file_details(reponame,filename)
     api_file_info = insert_file_details(file_details)
-
     return jsonify(api_file_info)
+
+
+@app.route('/fetch_file_info/<file_name>', methods=['GET'])
+def fetch_file_info(file_name):
+    file_name =search_file_details(file_name)
+    return jsonify(file_name)
+
 
 
 if __name__ == '__main__':
