62690e8c5d46ed225c436b6ab0e00a85d3f97055
maazil3861 <maazshaikh7020@gmail.com>
2020-08-05 13:12:01 +0530

    remove flag and use None to check identity

--- rest_app/job_datatime.py
+++ rest_app/job_datatime.py
@@ -15,26 +15,27 @@ class GitRepoApisDetails:
     def get_repo_details_by_month(self,file_name, file_created_year, file_created_month,  job_year, job_month, job_day, job_hr, job_min, job_sec,  job_interval_count):
 
         total_days=calendar.monthrange(file_created_year,file_created_month)[1]
-        flag=1
-        nextTime=''
+
+        nextTime = None
         for days in range(1,total_days+1):
 
             day_obj = datetime.date(file_created_year, file_created_month, days)
             target_url = "https://api.github.com/search/repositories?q={repo_name}+created:{date}".format(repo_name=file_name,date=day_obj)
-            if flag==1:
+
+            if nextTime is None:
                 job_run_time = dt.datetime(job_year, job_month, job_day, job_hr, job_min, job_sec)
                 nextTime =job_run_time + dt.timedelta(seconds=job_interval_count)
                 run_date = dt.datetime.strftime(nextTime, "%Y-%m-%d %H:%M:%S")
-                self.add_job_by_time(target_url,flag,run_date)
-                flag=0
+                self.add_job_by_time(target_url, nextTime, run_date)
+
             else:
                 nextTime = nextTime + dt.timedelta(seconds=job_interval_count)
                 run_date = dt.datetime.strftime(nextTime, "%Y-%m-%d %H:%M:%S")
-                self.add_job_by_time(target_url, flag, run_date)
+                self.add_job_by_time(target_url, nextTime, run_date)
 
-    def add_job_by_time(self, target_url, flag, run_date):
+    def add_job_by_time(self, target_url, nextTime, run_date):
 
-        if flag == 1:
+        if nextTime is None:
             print(run_date)
             sched.add_job(obj.job_is_get_repo, 'date', run_date=run_date, misfire_grace_time=10, args=[target_url])
         else:
@@ -44,6 +45,6 @@ class GitRepoApisDetails:
 
 obj=GitRepoApisDetails()
 # (file_name, file_created_year, file_created_month,job_year, job_month, job_day, job_hr, job_min, job_sec,  job_interval_count):
-obj.get_repo_details_by_month("dockerfile", 2020, 4, 2020, 8, 5, 13, 6, 10, 10)
+obj.get_repo_details_by_month("dockerfile", 2020, 4, 2020, 8, 5, 13, 10, 10, 10)
 
 sched.start()
