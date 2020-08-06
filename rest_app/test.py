45c4149440491273fd9958e42685f76f6a26473d
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2020-08-06 15:43:54 +0530

    if total count is > 1000 then get details by hour problem is solve

--- rest_app/test.py
+++ rest_app/test.py
@@ -39,8 +39,6 @@ Base.metadata.create_all(Engine)
 class GitRepoApisDetails:
 
     def job_is_get_repo(self, query_url, job_id):
-        print(query_url)
-        print(job_id)
 
         try:
 
@@ -49,7 +47,7 @@ class GitRepoApisDetails:
             self.matched_repositories = self.response.json()
             self.total_count = self.matched_repositories.get('total_count')
             print(self.total_count)
-
+            print(self.matched_repositories)
             if self.total_count == 0:
                 session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
                     {job_details_by_githubapi.Jobstatus: 'completed',
@@ -57,7 +55,84 @@ class GitRepoApisDetails:
                     synchronize_session=False)
                 session.commit()
 
+            elif self.total_count > 25 :
+
+                i = query_url.rfind(":")
+
+                year = int(query_url[i + 1:i + 5])
+                month = int(query_url[i + 6:i + 8])
+                day = int(query_url[i + 9:i + 11])
+
+                x = query_url.rfind("=")
+                z = query_url.rfind("+")
+                repo_name = query_url[x + 1:z]
+
+                repo_date1 = dt.datetime(year, month, day)
+
+                new_date1 = ''
+                new_date2 = ''
+                flag = True
+
+                for i in range(24):
+
+                    uid = uuid.uuid4().hex
+                    if flag == True:
+
+                        new_date1 = repo_date1 + dt.timedelta(seconds=0)
+                        first_date_is = dt.datetime.strftime(new_date1, "%Y-%m-%d")
+                        first_time_is = dt.datetime.strftime(new_date1, "%H:%M:%S")
+
+                        new_date2 = repo_date1 + dt.timedelta(hours=1)
+                        second_date_is = dt.datetime.strftime(new_date2, "%Y-%m-%d")
+                        second_time_is = dt.datetime.strftime(new_date2, "%H:%M:%S")
+
+                        target_url = "https://api.github.com/search/repositories?q={repo_name}+created:{first_date_is}T{first_time_is}..{second_date_is}T{second_time_is}".format(
+                            repo_name=repo_name, first_date_is=first_date_is, first_time_is=first_time_is,
+                            second_date_is=second_date_is, second_time_is=second_time_is)
+
+                        print(target_url)
+                        select_job_details = session.query(job_details_by_githubapi).order_by(
+                            desc(job_details_by_githubapi.CreatedAt))
+                        select_one_job = select_job_details.first()
+                        last_job_datetime = select_one_job.CreatedAt
+
+                        date_time_obj = dt.datetime.strptime(last_job_datetime, "%Y-%m-%d %H:%M:%S")
+                        nextTime = date_time_obj + dt.timedelta(seconds=10)
+                        run_date = dt.datetime.strftime(nextTime, "%Y-%m-%d %H:%M:%S")
+
+                        self.add_job_by_time(target_url,run_date,uid)
+                        flag = False
+
+                    else:
+
+                        first_date_is = dt.datetime.strftime(new_date2, "%Y-%m-%d")
+                        first_time_is = dt.datetime.strftime(new_date2, "%H:%M:%S")
+
+                        new_date2 = new_date2 + dt.timedelta(hours=1)
+                        second_date_is = dt.datetime.strftime(new_date2, "%Y-%m-%d")
+                        second_time_is = dt.datetime.strftime(new_date2, "%H:%M:%S")
+
+                        target_url = "https://api.github.com/search/repositories?q={repo_name}+created:{first_date_is}T{first_time_is}..{second_date_is}T{second_time_is}".format(
+                            repo_name=repo_name, first_date_is=first_date_is, first_time_is=first_time_is,
+                            second_date_is=second_date_is, second_time_is=second_time_is)
+
+                        print(target_url)
+                        select_job_details = session.query(job_details_by_githubapi).order_by(
+                            desc(job_details_by_githubapi.CreatedAt))
+                        select_one_job = select_job_details.first()
+                        last_job_datetime = select_one_job.CreatedAt
+
+                        date_time_obj = dt.datetime.strptime(last_job_datetime, "%Y-%m-%d %H:%M:%S")
+                        nextTime = date_time_obj + dt.timedelta(seconds=10)
+                        run_date = dt.datetime.strftime(nextTime, "%Y-%m-%d %H:%M:%S")
+
+
+                        self.add_job_by_time(target_url, run_date, uid)
+
+
+
             else:
+
                 session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
                     {job_details_by_githubapi.Jobstatus: 'completed',
                      job_details_by_githubapi.Joblog: 'get repository details'},
@@ -98,30 +173,38 @@ class GitRepoApisDetails:
 
         except ConnectionError as exception_msg:
 
-            select_rows = session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id)
-            count_fails_job = int(select_rows.first()) + 1
-
             session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
                 {job_details_by_githubapi.Jobstatus: 'not_completed',
-                 job_details_by_githubapi.Joblog: str(exception_msg),
-                 job_details_by_githubapi.Countfailjob: str(count_fails_job)}, synchronize_session=False)
+                 job_details_by_githubapi.Joblog: str(exception_msg)}, synchronize_session=False)
 
             session.commit()
 
-            self.re_add_fail_jobs(job_id, query_url, count_fails_job)
+            self.re_add_fail_jobs(job_id, query_url)
 
-    def re_add_fail_jobs(self, job_id, query_url, count_fails_job):
+    def re_add_fail_jobs(self, job_id, query_url):
 
-        if count_fails_job > 3:
+        select_rows = session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id)
+        fails_job = select_rows.first()
+        count_fails_job = int(fails_job.Countfailjob) + 1
+
+        if count_fails_job > 4:
 
             print("Job_id:=", job_id)
             print("Above job failed more than 3 times")
 
+            session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
+                {job_details_by_githubapi.Jobstatus: 'This job retry more then 3 time',
+                 job_details_by_githubapi.Joblog: str(self.matched_repositories),
+                 job_details_by_githubapi.Countfailjob: str(count_fails_job-1)},
+                synchronize_session=False)
+            session.commit()
+
+
         else:
 
             uid = uuid.uuid4().hex
-            session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
-                {job_details_by_githubapi.Countfailjob: str(count_fails_job)}, synchronize_session=False)
+            # session.query(job_details_by_githubapi).filter(job_details_by_githubapi.JobId == job_id).update(
+            #     {job_details_by_githubapi.Countfailjob: str(count_fails_job)}, synchronize_session=False)
 
             select_job_details = session.query(job_details_by_githubapi).order_by(
                 desc(job_details_by_githubapi.CreatedAt))
@@ -129,7 +212,7 @@ class GitRepoApisDetails:
             last_job_datetime = select_one_job.CreatedAt
 
             date_time_obj = dt.datetime.strptime(last_job_datetime, "%Y-%m-%d %H:%M:%S")
-            nextTime = date_time_obj + dt.timedelta(seconds=10)
+            nextTime = date_time_obj + dt.timedelta(seconds=3)
             run_date = dt.datetime.strftime(nextTime, "%Y-%m-%d %H:%M:%S")
 
             sched.add_job(obj.job_is_get_repo, 'date', run_date=run_date, misfire_grace_time=10, args=[query_url, uid],
@@ -143,7 +226,7 @@ class GitRepoApisDetails:
 
             github_repo_api = job_details_by_githubapi(JobId=uid, JobType='github', CreatedAt=run_date,
                                                        UpdatedAt='', JobObject=job_details_json,
-                                                       Jobstatus='pending', Joblog='job log', previousjobid=job_id)
+                                                       Jobstatus='pending', Joblog='job log', previousjobid=job_id,Countfailjob=count_fails_job)
 
             session.add(github_repo_api)
             session.commit()
@@ -162,7 +245,7 @@ class GitRepoApisDetails:
         else:
             total_days = calendar.monthrange(file_created_year, file_created_month)[1]
             nextTime = None
-            for days in range(1, total_days + 1):
+            for days in range(20, total_days + 1):
 
                 day_obj = datetime.date(file_created_year, file_created_month, days)
                 target_url = "https://api.github.com/search/repositories?q={repo_name}+created:{date}".format(
@@ -201,6 +284,6 @@ class GitRepoApisDetails:
 
 obj = GitRepoApisDetails()
 # (file_name, file_created_year, file_created_month,job_year, job_month, job_day, job_hr, job_min, job_sec,  job_interval_count):
-obj.get_repo_details_by_month("dockerfile", 2020, 4, 2020, 8, 5, 19, 24, 00, 10)
+obj.get_repo_details_by_month("dockerfile", 2020, 4, 2020, 8, 6, 15 , 34 , 00, 10)
 
 sched.start()
