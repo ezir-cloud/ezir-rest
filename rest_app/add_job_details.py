70c8d81b9ff84456afc240ea2feeb1a0a899f4aa
maazil3861 <maazshaikh7020@gmail.com>
2020-08-07 23:24:33 +0530

    add job details

---
+++ rest_app/add_job_details.py
@@ -0,0 +1,9 @@
+
+class JobDetails():
+        job_year  = 2020
+        job_month = 8
+        job_day  =  7
+        job_hr = 22
+        job_min =  12
+        job_sec = 45
+        job_interval_count = 1
