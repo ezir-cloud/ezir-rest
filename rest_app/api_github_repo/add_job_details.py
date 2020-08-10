39e62e563ce2b3434e7e47913cee7240691ee3ef
maazil3861 <maazshaikh7020@gmail.com>
2020-08-10 21:05:08 +0530

    job datetime

--- rest_app/api_github_repo/add_job_details.py
+++ rest_app/api_github_repo/add_job_details.py
@@ -3,7 +3,7 @@ class JobDetails():
         job_year  = 2020
         job_month = 8
         job_day  =  10
-        job_hr =  9
-        job_min =  40
-        job_sec = 35
+        job_hr =  21
+        job_min =  3
+        job_sec = 0
         job_interval_count = 1
