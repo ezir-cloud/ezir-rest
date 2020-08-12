ede03a41583ea90a0cf40ce7c6a9cd623c13463e
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2020-08-12 21:43:45 +0530

    searching problem is solved

--- rest_app/view/views.py
+++ rest_app/view/views.py
@@ -1,17 +1,24 @@
 from flask import Flask, jsonify, request
-from githubapis.search import GitRepositoryApisDetails
-from githubapis.search import Github
-from rest_app.controller.api_for_repo_info import api_for_repo_details, insert_repo_details
+from rest_app.controller.api_for_repo_info import api_for_repo_details, insert_repo_details,get_repo_details
 
 app = Flask(__name__)
 
 @app.route('/repo-details', methods=['POST'])
 def repo_details():
-    reponame = request.args.get('reponame')
 
+    reponame = request.args.get('reponame')
     repo_details = api_for_repo_details(reponame)
-    api_repo_info = insert_repo_details(repo_details)
-    return jsonify(api_repo_info)
+    insert_repo_details(repo_details)
+
+
+@app.route('/fatch-repo-details/<repo_name>', methods=['GET'])
+def fetch_repo_details(repo_name):
+
+    details=get_repo_details(repo_name)
+    return jsonify(details)
+
 
 if __name__ == '__main__':
     app.run (debug = True)
+
+
