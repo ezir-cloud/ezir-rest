6b2053b74013f713bdaf4b1ba6ea27bdfcafc3f3
Maaz <34999445+MaazMS@users.noreply.github.com>
2020-08-17 19:46:10 +0530

    add jobstore and start apscheduler

--- add_job_running_scheduler/scheduler_start.py
+++ add_job_running_scheduler/scheduler_start.py
@@ -1,15 +1,30 @@
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
-scheduler = BlockingScheduler()
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from pytz import utc
 
-database_url ='sqlite:///job_store.sqlite'
-scheduler.add_jobstore('sqlalchemy', url=database_url)
+
+jobstores = {
+    'default': SQLAlchemyJobStore(url='sqlite:///job_store.sqlite',tablename='apscheduler_jobs')
+}
+executors ={
+        'default': ThreadPoolExecutor(20),
+        'processpool': ProcessPoolExecutor(5)
+    }
+job_defaults ={
+        'coalesce': False,
+        'max_instances': 3
+    }
+
+scheduler = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+print("scheudler_start: ", id(scheduler))
+
 
 def add_job_in_jobstore():
     print ("Hello")
 
-job_obj=scheduler.add_job(add_job_in_jobstore, 'interval', seconds=59)
+job_obj=scheduler.add_job(add_job_in_jobstore,  'interval', seconds=5)
 print(job_obj.id)
-
 
 if __name__ == '__main__':
     print("scheduler starting...")
