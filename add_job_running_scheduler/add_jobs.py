14431ac81b97da686a9f4840cdce398ebd25359a
Maaz <34999445+MaazMS@users.noreply.github.com>
2020-08-17 19:47:00 +0530

    add job and start the apscheduler

--- add_job_running_scheduler/add_jobs.py
+++ add_job_running_scheduler/add_jobs.py
@@ -1,14 +1,11 @@
 from add_job_running_scheduler.scheduler_start import scheduler
 
-print("scheduler state: ", scheduler.state)
+print("add_jobs: ", id(scheduler))
+print(scheduler.state)
+
 def say_hello_job():
-    print ("Hello")
+    print ("Hello1")
 
-
-job_obj=scheduler.add_job(say_hello_job, 'interval', seconds=3)
-print(job_obj)
+job_obj=scheduler.add_job(say_hello_job, 'interval', seconds=10)
 print(job_obj.id)
-
-jobs = scheduler.get_jobs()
-print(jobs)
-
+scheduler.start()
